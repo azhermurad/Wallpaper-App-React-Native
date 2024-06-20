5b2c8aa0cd6624f8d5372c57da039e2860e2715d
azher ali <azhermurad@gmail.com>
2024-06-20 15:10:43 +0000

    add into screen

---
+++ components/Intro.tsx
@@ -0,0 +1,47 @@
+import { View, Text, Dimensions, Button, ImageBackground } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import Animated, { FadeInDown } from 'react-native-reanimated';
+import CustomButton from './ui/CustomButton';
+import { hp } from '@/util/responseUnit';
+import { Colors } from '@/constants/Colors';
+
+const Intro = () => {
+    const { width, height } = Dimensions.get('screen'); 
+    return (
+        <ImageBackground
+            source={require('../assets/images/into.jpg')}
+            style={{
+                width: width,
+                height: height,
+            }}
+            resizeMode='cover'
+        >
+            <LinearGradient
+                // Button Linear Gradient
+                style={{
+                    width: width,
+                    height: height,
+                    position: 'relative',
+                    alignItems: 'center',
+                    justifyContent: 'flex-end',
+                }}
+                colors={['transparent', 'white']}
+                start={{ x: 0.1, y: 0.2 }}
+                end={{x:0.1,y:0.9}}
+            >
+                <Animated.View
+                    entering={FadeInDown.duration(400).springify().damping(20)}
+                    style={{
+                        position: 'absolute',
+                        width: '90%',
+                        bottom:hp(5)
+                    }}
+                >
+                    <CustomButton  backgroundColor={Colors.skyBlue200} onpress={() => {}} title="let's explorer" />
+                </Animated.View>
+            </LinearGradient>
+        </ImageBackground>
+    );
+};
+
+export default Intro;
---
+++ components/ui/CustomButton.tsx
@@ -0,0 +1,83 @@
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    TextProps,
+    StyleSheet,
+} from 'react-native';
+import React from 'react';
+import { hp } from '@/util/responseUnit';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Colors } from '@/constants/Colors';
+
+type IPrpos = {
+    title: string;
+    backgroundColor?: string;
+    color?: string;
+    iconName?: keyof typeof Ionicons.glyphMap;
+    iconColor?: string;
+    iconSize?: number;
+    IconShow?: boolean;
+    onpress: () => void;
+};
+
+const CustomButton = ({
+    title,
+    iconName = 'arrow-forward-circle-outline',
+    iconColor = 'white',
+    IconShow = true,
+    iconSize = 25,
+    onpress,
+    backgroundColor = 'orange',
+}: IPrpos) => {
+    return (
+        <View style={styles.button}> 
+            <TouchableOpacity
+                style={{
+                    paddingVertical: 10,
+                    paddingHorizontal: 20,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    flexDirection: 'row',
+                    backgroundColor: backgroundColor,
+                    shadowOffset: { width: 0, height: 3 },
+                    shadowOpacity: 0.8,
+                    shadowRadius: 2,
+                    elevation: 3,
+                }}
+                onPress={onpress}
+            >
+                <Text style={styles.text}>{title}</Text>
+
+                {IconShow && (
+                    <Ionicons
+                        style={{ fontWeight: 'bold' }}
+                        name={iconName}
+                        size={iconSize}
+                        color={iconColor}
+                    />
+                )}
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    button: {
+        borderRadius: 30,
+        overflow: 'hidden',
+        elevation:3,
+        borderWidth:4,
+        borderColor:Colors.skyBlue100
+
+    },
+    text: {
+        color: '#FFFFFF',
+        fontSize: hp(3),
+        fontWeight: 'bold',
+        marginRight: 10,
+        textTransform: 'capitalize',
+    },
+});
+
+export default CustomButton;
